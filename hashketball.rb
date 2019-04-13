4c60a534aaf092f94a6ec63c0926933037a594ed
Qun Li Huang <qunli02@gmail.com>
2019-04-13 05:13:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -21,14 +21,14 @@ def game_hash
                                 blocks:12
                                 slam_dunks:7
                               }
-                  "player" => { number:
-                                shoe:
-                                points:
-                                rebounds:
-                                assists:
-                                steals:
-                                blocks:
-                                slam_dunks:
+                  "Brook Lopez" => { number:11
+                                shoe:17
+                                points:17
+                                rebounds:19
+                                assists:10
+                                steals:3
+                                blocks:1
+                                slam_dunks:15
                               }
                   "player" => { number:
                                 shoe:
