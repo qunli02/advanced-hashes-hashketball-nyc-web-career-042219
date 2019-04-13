d692e93d2d575e888e9d4a0c4735813a99f4cdd8
Qun Li Huang <qunli02@gmail.com>
2019-04-13 05:14:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,14 +30,14 @@ def game_hash
                                 blocks:1
                                 slam_dunks:15
                               }
-                  "Mason Plumlee" => { number:
-                                shoe:
-                                points:
-                                rebounds:
-                                assists:
-                                steals:
-                                blocks:
-                                slam_dunks:
+                  "Mason Plumlee" => { number:1
+                                shoe:19
+                                points:26
+                                rebounds:12
+                                assists:6
+                                steals:3
+                                blocks:8
+                                slam_dunks:5
                               }
                   "player" => { number:
                                 shoe:
