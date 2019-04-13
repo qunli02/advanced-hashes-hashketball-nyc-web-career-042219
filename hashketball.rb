90240732ec3ed0fcdaa9fa48b4a6f415e6cdf507
Qun Li Huang <qunli02@gmail.com>
2019-04-13 05:17:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -62,10 +62,10 @@ def game_hash
                                 blocks:7
                                 slam_dunks:2
                               }
-                  "Bismak Biyombo" => { number:
-                                shoe:
-                                points:
-                                rebounds:
+                  "Bismak Biyombo" => { number:0
+                                shoe:16
+                                points:12
+                                rebounds:4
                                 assists:
                                 steals:
                                 blocks:
