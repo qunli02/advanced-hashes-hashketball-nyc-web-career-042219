074f9b13d326edab9cefd3a0133b59e2e751d4c6
Qun Li Huang <qunli02@gmail.com>
2019-04-13 04:43:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,14 +3,14 @@ def game_hash
   { home:
       { team_name: "Brooklyn Nets"
         color: [Black, White]
-        player: { "player" => { number:
-                                shoe:
-                                points:
-                                rebounds:
-                                assists:
-                                steals:
-                                blocks:
-                                slam_dunks:
+        player: { "Alan Anderson" => { number: 0
+                                shoe:16
+                                points:22
+                                rebounds:12
+                                assists:12
+                                steals:3
+                                blocks:1
+                                slam_dunks:1
                               }
                   "player" => { number:
                                 shoe:
