587ddf7767c8cc5f951813b451c5c8c839849e45
Qun Li Huang <qunli02@gmail.com>
2019-04-13 05:44:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,8 +10,8 @@ def game_hash
                                 assists:12,
                                 steals:3,
                                 blocks:1,
-                                slam_dunks:1,
-                              }
+                                slam_dunks:1
+                              },
                   "Reggie Evans" => { number:30,
                                 shoe:14,
                                 points:12,
@@ -19,8 +19,8 @@ def game_hash
                                 assists:12,
                                 steals:12,
                                 blocks:12,
-                                slam_dunks:7,
-                              }
+                                slam_dunks:7
+                              },
                   "Brook Lopez" => { number:11,
                                 shoe:17,
                                 points:17,
@@ -28,8 +28,8 @@ def game_hash
                                 assists:10,
                                 steals:3,
                                 blocks:1,
-                                slam_dunks:15,
-                              }
+                                slam_dunks:15
+                              },
                   "Mason Plumlee" => { number:1,
                                 shoe:19,
                                 points:26,
@@ -37,8 +37,8 @@ def game_hash
                                 assists:6,
                                 steals:3,
                                 blocks:8,
-                                slam_dunks:5,
-                              }
+                                slam_dunks:5
+                              },
                   "Jason Terry" => { number:31,
                                 shoe:15,
                                 points:19,
