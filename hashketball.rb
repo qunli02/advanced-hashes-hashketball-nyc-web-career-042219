05095bd00be39c91d9e54b3f26bc5b6d87e72c71
Qun Li Huang <qunli02@gmail.com>
2019-04-13 04:39:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,19 @@
 # Write your code here!
-
+def game_hash 
+  { home:
+      { team_name: " "
+        color: []
+        player: { "player" => 
+                  
+                }
+      }
+    away:
+      { team_name: " "
+        color: []
+        player:
+      }
+  }
+end
 
 
 
